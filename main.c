e1f19487ce52af4b7f54716006fa6ca455ac3724
Finn Brady <finn.t.brady@gmail.com>
2022-06-28 20:28:04 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define options 7
+
+#include "RPS.h"
+
+int main() {
+	int n=options,m=options;
+	while(n--){
+		m=options;
+		while(m--){
+			printf("COMP(%i,%i): %i/%i\n",n,m,RPSComp(n, m),RPSCompV(n, m));
+		}
+		printf("\n");
+	}
+	
+	int* Bal=biasCheck();
+	int* BalV=biasCheckV();
+	printf("\nBias:%i/%i",*Bal,*BalV);
+
+	n = options-1;
+
+	while(n--){
+		Bal++;
+		BalV++;
+		printf(",%i/%i",*Bal,*BalV);
+	}
+	printf("\n");
+
+	return 0;
+}
