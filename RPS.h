e0b4c8ababaddf526691fb9fdb13453d72ac3f8f
Finn Brady <finn.t.brady@gmail.com>
2023-01-14 02:38:16 +0000

    Code readability
    Added comments
    Changed the name of macro defined value `options` to `RPSoptions` to reduce likelyhood of conflict

--- RPS.h
+++ RPS.h
@@ -1,13 +1,19 @@
-//macro define 'options' before including file
+//This library is used to compare enumerated values in an unbiased RockPaperScissors-esque system
+//The enumerated values should be based on a graph aranged in such a way slicing it across from one option cleanly divided it into groups of options that win and lose against the option chosen
+//The afformentioned graph is possible for any unbiased RPS-like system
 
-#ifndef options
-#define options 7
+//macro define 'RPSoptions' before including file
+#ifndef RPSoptions
+#define RPSoptions 7 //this logic was originally made for a system with 7 options, so using it as the default felt right
 #endif
 
-#define vicLim (options/2 - (options/2)%1)
+//I think I named this as an abreviation of victim limit, meaning the amount of options one option can beat
+#define vicLim (RPSoptions / 2 - (RPSoptions / 2) % 1)
 
-#define rotate(a,b) (b+(options-a))%options
+//named for the graph idea mentioned earlier, this changes the enumerated value of b as though a was 0
+#define rotate(a,b) (b + (RPSoptions - a)) % RPSoptions
 
+//Compares 2 enumerated options for RPS-like system
 int RPSComp(int home,int away){
 	if(home==away) return 0;//draw
 	
@@ -18,17 +24,24 @@ int RPSComp(int home,int away){
 	return 2*(check<=0)-1;
 }
 
-//Verbose version for dealing with even options
+//Verbose version for dealing with even options. incase that's ever somehow needed
 int RPSCompV(int home, int away){
 	return (RPSComp(home,away)-RPSComp(away,home))/2;
-
+	//yes, this function does try it both ways as though it were an odd N system
+	//and then if the outcomes contradict it determines a draw
 }
 
+
+//Bias checking functions, mainly for testing purposes
+//if the answer comes up 0 then everything's working correctly
+
+//Bias check for an odd N system
+//trying this for an even N system should give a none-zero answer
 int* biasCheck(){
-	int n=options,m=options;
-	int* balance = calloc(options+1,8);
+	int n=RPSoptions,m;
+	int* balance = calloc(RPSoptions+1,8);
 	while(n--){
-		m=options;
+		m=RPSoptions;
 		while(m--){
 			balance[n]+=RPSComp(n,m);
 		}
@@ -36,11 +49,13 @@ int* biasCheck(){
 	return balance;
 }
 
+//Bias check for even N systems
+//should always come out as 0
 int* biasCheckV(){
-	int n=options,m=options;
-	int* balance = calloc(options+1,8);
+	int n=RPSoptions,m;
+	int* balance = calloc(RPSoptions+1,8);
 	while(n--){
-		m=options;
+		m=RPSoptions;
 		while(m--){
 			balance[n]+=RPSCompV(n,m);
 		}
